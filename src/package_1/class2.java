bf679de1a28ec6cefbccd5e9d93adc1dcd5a0aa3
Vlad Duplyakin <DuplyakinV@gmail.com>
2016-09-27 11:15:38 +0300

    commit from 2 project in master

---
+++ src/package_1/class2.java
@@ -0,0 +1,10 @@
+package package_1;
+
+/**
+ * Created by Vlad on 27.09.2016.
+ */
+public class class2 {
+    public static void main(String[] args) {
+        System.out.println("1project: changes in branch: master\"");
+    }
+}
