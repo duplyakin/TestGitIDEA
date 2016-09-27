06a773c9de7bfbdfdd2aab23a6adecc97910cfd7
Vlad Duplyakin <DuplyakinV@gmail.com>
2016-09-27 09:56:32 +0300

    Initial commit

---
+++ src/package_1/main.java
@@ -0,0 +1,10 @@
+package package_1;
+
+/**
+ * Created by Vlad on 27.09.2016.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
