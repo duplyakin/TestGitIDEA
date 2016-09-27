477d26eb062dac46fd2606d7e4390707dbf9d656
Vlad Duplyakin <DuplyakinV@gmail.com>
2016-09-27 11:08:04 +0300

    changed master from 2 project

---
+++ src/package_1/class1.java
@@ -0,0 +1,10 @@
+package package_1;
+
+/**
+ * Created by Vlad on 27.09.2016.
+ */
+public class class1 {
+    public static void main(String[] args) {
+        System.out.println("2project: changes in branch: master");
+    }
+}
