4d7c857f2225b80f104c265c0f50754fc8be6bc1
Vlad Duplyakin <DuplyakinV@gmail.com>
2016-11-23 14:48:52 +0300

    commit from 1 project in master

--- src/package_1/main.java
+++ src/package_1/main.java
@@ -8,5 +8,6 @@ public class main {
         System.out.println("Hello world!");
         System.out.println("1");
         System.out.println("my_branch_1");
+        System.out.println("master_1");
     }
 }
